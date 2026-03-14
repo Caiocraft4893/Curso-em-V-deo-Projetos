495ec2f99c0afd409a85c775b0954c769e7cab1b
Caiocraft4893 <caiotavaresdefranca@gmail.com>
2026-03-13 21:11:19 -0300

    Delete aula11 directory

--- aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var país = 'Brasil'
-console.log(`Vivendo em ${país.valueOf()}`)
-if (país.valueOf() == 'Brasil') {
-    console.log('Você é Brasileiro!')
-} else {
-    console.log('Você é Estrangeiro!')
-}
